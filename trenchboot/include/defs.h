d7459297a7a92cf82fd9ec86965fb731fb1aa9c8
Ross Philipson <ross.philipson@oracle.com>
2018-08-21 17:00:13 -0400

    Added init address code and L1/L2 page tables.
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/include/defs.h
+++ trenchboot/include/defs.h
@@ -19,4 +19,22 @@
 #define __packed        __attribute__ ((packed))
 #define __maybe_unused  __attribute__ ((unused))
 
+/* MSRs */
+
+#define MSR_EFER  0xc0000080
+#define VM_CR_MSR 0xc0010114
+
+/* EFER bits */
+#define EFER_SCE 0  /* SYSCALL/SYSRET */
+#define EFER_LME 8  /* Long Mode enable */
+#define EFER_LMA 10 /* Long Mode Active (read-only) */
+#define EFER_NXE 11  /* no execute */
+#define EFER_SVME 12   /* SVM extensions enable */
+
+/* VM CR MSR bits */
+#define VM_CR_DPD 0
+#define VM_CR_R_INIT 1
+#define VM_CR_DIS_A20M 2
+#define VM_CR_SVME_DISABLE 4
+
 #endif /* __DEFS_H__ */
