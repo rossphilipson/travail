a2d2d693dce0b38b839fd3e746600f8a37f977dc
Ross Philipson <ross.philipson@oracle.com>
2018-08-21 20:00:03 -0400

    Arg and comment in setup() start routine.
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/setup.c
+++ trenchboot/setup.c
@@ -1,3 +1,11 @@
-void setup(void)
+void setup(void *lz_base)
 {
+    /*
+     * TODO we are in 64b mode, paging is setup. This is the launching
+     * point. We can now do what we want. First order of business is to setup
+     * DEV to cover memory from LZ_SECOND_STAGE_STACK_OFFSET and load a
+     * bigger stack...
+     * ... When all is done, drop back to 32b protected mode, paging off and
+     * trampoline to the kernel.
+     */
 }
