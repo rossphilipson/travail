92038c60820f51c8f2bcdcaf982d0bfd410a64fb
Ross Philipson <ross.philipson@oracle.com>
2018-11-19 11:49:48 -0500

    Add the basic uintN_t types
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/lz/include/types.h
+++ trenchboot/lz/include/types.h
@@ -17,6 +17,11 @@ typedef unsigned long          uintptr_t;
 typedef unsigned long	size_t;
 typedef long		ssize_t;
 
+typedef unsigned char		uint8_t;
+typedef unsigned short		uint16_t;
+typedef unsigned int		uint32_t;
+typedef unsigned long long	uint64_t;
+
 #define NULL 0
 
 #define min(x,y) ({ \
