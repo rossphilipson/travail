fe8b3089657d2e20817ac80b2565ffdb645f4a8d
Ross Philipson <ross.philipson@oracle.com>
2018-11-21 14:04:54 -0500

    Add GPLv2 license file and source headers
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/lz/include/boot.h
+++ trenchboot/lz/include/boot.h
@@ -1,3 +1,19 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 /* -*- linux-c -*- ------------------------------------------------------- *
  *
  *   Copyright (C) 1991, 1992 Linus Torvalds
@@ -24,14 +40,14 @@
 #define __BOOT_H__
 
 typedef struct __packed sl_header {
-    u16 lz_offet;
-    u16 lz_length;
+	u16 lz_offet;
+	u16 lz_length;
 } sl_header_t;
 
 typedef struct __packed lz_header {
-    u32 trenchboot_loader_size;
-    u32 zero_page_addr;
-    u8  msb_key_hash[20];
+	u32 trenchboot_loader_size;
+	u32 zero_page_addr;
+	u8  msb_key_hash[20];
 } lz_header_t;
 
 /* Basic port I/O */
--- trenchboot/lz/include/sha1sum.h
+++ trenchboot/lz/include/sha1sum.h
@@ -1,3 +1,22 @@
+/* sha1sum.c - print SHA-1 Message-Digest Algorithm
+ * Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
+ * Copyright (C) 2004 g10 Code GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #ifndef __SHA1SUM_H__
 #define __SHA1SUM_H__
 
