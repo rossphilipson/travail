5062100f97d6d87bed7c457f7eab1c5b891672db
Ross Philipson <ross.philipson@oracle.com>
2019-01-21 12:29:14 -0500

    Added a script to pull down all the SINITs
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

---
+++ trenchboot/sinit/getsinits.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+OPENXT_MIRROR="https://openxt.ainfosec.com/mirror"
+
+wget "${OPENXT_MIRROR}/GM45_GS45_PM45-SINIT_51.zip"
+wget "${OPENXT_MIRROR}/Q45_Q43-SINIT_51.zip"
+wget "${OPENXT_MIRROR}/Q35-SINIT_51.zip"
+wget "${OPENXT_MIRROR}/i5_i7_DUAL-SINIT_51.zip"
+wget "${OPENXT_MIRROR}/i7_QUAD-SINIT_51.zip"
+wget "${OPENXT_MIRROR}/Xeon-5600-3500-SINIT-v1.1.zip"
+wget "${OPENXT_MIRROR}/Xeon-E7-8800-4800-2800-SINIT-v1.1.zip"
+wget "${OPENXT_MIRROR}/3rd-gen-i5-i7-sinit-67.zip"
+wget "${OPENXT_MIRROR}/4th-gen-i5-i7-sinit-75.zip"
+wget "${OPENXT_MIRROR}/5th_gen_i5_i7-SINIT_79.zip"
+wget "${OPENXT_MIRROR}/6th_gen_i5_i7-SINIT_71.zip"
+wget "${OPENXT_MIRROR}/7th_gen_i5_i7-SINIT_74.zip"
+wget "${OPENXT_MIRROR}/8th_gen_i5_i7-SINIT_76.zip"
