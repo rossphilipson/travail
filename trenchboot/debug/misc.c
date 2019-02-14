8ae7bb28a38b608807c7d6e8ae17fcb8f08f5003
Ross Philipson <ross.philipson@oracle.com>
2019-02-14 12:06:37 -0500

    Use a pause loop for TXTCR_CMD_RESET and disable all caching for MMIO
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/debug/misc.c
+++ trenchboot/debug/misc.c
@@ -17,3 +17,39 @@
         printk(TBOOT_INFO"**^^$$E2STS: 0x%llx\n", e2sts);
     }
 
+/* Setup earlyprintk in sl_main() */
+extern char *vidmem;
+extern int vidport;
+extern int lines, cols;
+
+void sl_main(u8 *bootparams)
+{
+	struct sha1_state sctx = {0};
+	u8 sha1_hash[SHA1_DIGEST_SIZE];
+	u32 cmdline_len;
+	u64 cmdline_addr;
+	struct tpm *tpm;
+	int ret;
+
+	boot_params = bootparams;
+
+	sanitize_boot_params(boot_params);
+
+	if (boot_params->screen_info.orig_video_mode == 7) {
+		vidmem = (char *) 0xb0000;
+		vidport = 0x3b4;
+	} else {
+		vidmem = (char *) 0xb8000;
+		vidport = 0x3d4;
+	}
+
+	lines = boot_params->screen_info.orig_video_lines;
+	cols = boot_params->screen_info.orig_video_cols;
+
+	/* Move here from extract_kernel() */
+	console_init();
+	error_putstr("***RJP*** test string and hex:\n");
+	error_puthex(0xffaa2211);
+
+	...
+}
