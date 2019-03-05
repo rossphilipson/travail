846f21da048a9be8c6c6612cb07b929024471f4e
Ross Philipson <ross.philipson@oracle.com>
2019-03-05 15:54:18 -0500

    Latest TXT AP bringup code
    This is the second version that did not end up working because you
    cannot INIT an AP in SMX mode. It is also the basis for the third
    version that will very likely finally work.
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/slboot/include/txt/heap.h
+++ trenchboot/slboot/include/txt/heap.h
@@ -270,6 +270,7 @@ typedef struct __packed {
     uint64_t     saved_misc_enable_msr; /* saved prior to SENTER */
     mtrr_state_t saved_mtrr_state;      /* saved prior to changes for SINIT */
     uint64_t     lo_pmr_min;
+    uint64_t     ap_wake_ebp;
     uint8_t      event_log_buffer[MAX_EVENT_LOG_SIZE];
 } os_mle_data_t;
 
