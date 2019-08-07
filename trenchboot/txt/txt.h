72e478b62e483481fb077cee3c9dd5ea8c14a03b
Ross Philipson <ross.philipson@oracle.com>
2019-08-07 10:00:59 -0400

    Remove OS-MLE implementation specific bits in that structure
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/txt/txt.h
+++ trenchboot/txt/txt.h
@@ -229,11 +229,8 @@ struct txt_bios_data {
 	/* Ext Data Elements */
 } __attribute__((packed));
 
-#define TXT_MAX_EVENT_LOG_SIZE				5*4*1024   /* 4k*5 */
-
 struct txt_os_mle_data {
-	u32 zero_page_addr;
-	u8  event_log_buffer[TXT_MAX_EVENT_LOG_SIZE];
+	/* This structure is implementation specific */
 } __attribute__((packed));
 
 struct txt_os_sinit_data {
