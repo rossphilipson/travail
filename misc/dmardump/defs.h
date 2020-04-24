003a0e77258683302408d272558c21d310eabbad
Ross Philipson <ross.philipson@oracle.com>
2020-04-24 14:14:05 -0400

    Publish my DMAR dump utility
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

---
+++ misc/dmardump/defs.h
@@ -0,0 +1,20 @@
+#ifndef __DEFS_H__
+#define __DEFS_H__
+
+#if __WORDSIZE == 64
+#define INT_FMT "%ld"
+#define UINT_FMT "%lx"
+#else
+#define INT_FMT "%d"
+#define UINT_FMT "%x"
+#endif
+
+#define ACPI_SIG_DMAR           "DMAR"	/* DMA Remapping table */
+
+uint8_t *helper_mmap(size_t phys_addr, size_t length);
+void helper_unmmap(uint8_t *addr, size_t length);
+int helper_efi_locate(const char *efi_entry, uint32_t length, size_t *location);
+
+int acpi_get_dmar(uint8_t *dmar_buf, uint32_t length);
+
+#endif /* __DEFS_H__ */
---
+++ misc/dmardump/utils.c
@@ -0,0 +1,67 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "defs.h"
+
+#define EFI_LINE_SIZE 64
+
+uint8_t *helper_mmap(size_t phys_addr, size_t length)
+{
+    uint32_t page_offset = phys_addr % sysconf(_SC_PAGESIZE);
+    uint8_t *addr;
+    int fd;
+
+    fd = open("/dev/mem", O_RDONLY);
+    if (fd == -1)
+        return NULL;
+
+    addr = (uint8_t*)mmap(0, page_offset + length, PROT_READ, MAP_PRIVATE, fd, phys_addr - page_offset);
+    close(fd);
+
+    if (addr == MAP_FAILED)
+        return NULL;
+
+    return addr + page_offset;
+}
+
+void helper_unmmap(uint8_t *addr, size_t length)
+{
+    uint32_t page_offset = (size_t)addr % sysconf(_SC_PAGESIZE);
+
+    munmap(addr - page_offset, length + page_offset);
+}
+
+int helper_efi_locate(const char *efi_entry, uint32_t length, size_t *location)
+{
+    FILE *systab = NULL;
+    char efiline[EFI_LINE_SIZE];
+    char *val;
+    off_t loc = 0;
+
+    *location = 0;
+
+    /* use EFI tables if present */
+    systab = fopen("/sys/firmware/efi/systab", "r");
+    if (systab != NULL) {
+        while((fgets(efiline, EFI_LINE_SIZE - 1, systab)) != NULL) {
+            if (strncmp(efiline, efi_entry, 6) == 0) {
+                /* found EFI entry, get the associated value */
+                val = memchr(efiline, '=', strlen(efiline)) + 1;
+                loc = strtol(val, NULL, 0);
+                break;
+            }
+        }
+        fclose(systab);
+
+        if (loc != 0) {
+        	*location = loc;
+            return 0;
+        }
+    }
+
+    return -1;
+}
