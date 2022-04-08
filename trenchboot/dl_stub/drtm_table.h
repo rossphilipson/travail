6328f121b24636d1ac85446a23680f361416d758
Ross Philipson <ross.philipson@oracle.com>
2022-04-08 11:27:48 -0400

    Pass in table head
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/dl_stub/drtm_table.h
+++ trenchboot/dl_stub/drtm_table.h
@@ -11,16 +11,13 @@
 /* Put this in efi.h if it becomes a standard */
 #define DRTM_TABLE_GUID				EFI_GUID(0x877a9b2a, 0x0385, 0x45d1, 0xa0, 0x34, 0x9d, 0xac, 0x9c, 0x9e, 0x56, 0x5f)
 
-/* General entrys */
+/* General entries */
 #define DRTM_TABLE_HEADER		0x0000	/* Always first */
 #define DRTM_ENTRY_END			0xffff
 
-/* DRTM general entry */
+/* DRTM general entries */
 #define DRTM_ENTRY_ARCHITECTURE		0x0001
-
-/* DCE entrys */
-#define DRTM_ENTRY_DCE_CLASS		0x10
-#define DRTM_ENTRY_DCE_INFO		0x01
+#define DRTM_ENTRY_DCE_INFO		0x0002
 
 struct drtm_entry_hdr {
 	u16 type;
@@ -49,17 +46,18 @@ struct drtm_entry_dce_info {
 	u32 dce_size;
 } __packed;
 
-static inline void *drtm_end_of_entrys(void)
+static inline void *drtm_end_of_entrys(struct drtm_entry_hdr *head)
 {
 	/* TODO not sure what this is yet */
 	/*return (((void *) &bootloader_data) + bootloader_data.size);*/
 	return 256;
 }
 
-static inline void *drtm_next_entry(struct drtm_entry_hdr *entry)
+static inline void *drtm_next_entry(struct drtm_entry_hdr *head,
+				    struct drtm_entry_hdr *curr)
 {
-	void *next = entry + entry->len;
-	return next < drtm_end_of_entrys() ? x : NULL;
+	void *next = curr + curr->len;
+	return next < drtm_end_of_entrys(head) ? x : NULL;
 }
 
 static inline void *drtm_next_of_type(struct drtm_entry_hdr *entry, u8 type)
