f1074aa964c071ab313f1af26b53ae171e46e617
Ross Philipson <ross.philipson@oracle.com>
2024-04-08 21:20:17 -0700

    Fixes to format for debug memmap priting
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/debug/debug_memmap.h
+++ trenchboot/debug/debug_memmap.h
@@ -21,15 +21,15 @@ static void e820_print_type_dbg(unsigned int type)
 {
 	switch (type) {
 	case E820_TYPE_RAM_DBG:			/* Fall through */
-	case E820_TYPE_RESERVED_KERN_DBG:	printd_str("usable\n");			break;
-	case E820_TYPE_RESERVED_DBG:		printd_str("reserved\n");		break;
-	case E820_TYPE_SOFT_RESERVED_DBG:	printd_str("soft reserved\n");		break;
-	case E820_TYPE_ACPI_DBG:		printd_str("ACPI data\n");		break;
-	case E820_TYPE_NVS_DBG:			printd_str("ACPI NVS\n");		break;
-	case E820_TYPE_UNUSABLE_DBG:		printd_str("unusable\n");		break;
-	case E820_TYPE_PMEM_DBG:		/* Fall through_DBG: */
-	case E820_TYPE_PRAM_DBG:		printd_str("persistent mem\n");		break;
-	default:				printd_str("persistent RAM \n");	break;
+	case E820_TYPE_RESERVED_KERN_DBG:	printd_str("usable\n");		break;
+	case E820_TYPE_RESERVED_DBG:		printd_str("reserved\n");	break;
+	case E820_TYPE_SOFT_RESERVED_DBG:	printd_str("soft reserved\n");	break;
+	case E820_TYPE_ACPI_DBG:		printd_str("ACPI data\n");	break;
+	case E820_TYPE_NVS_DBG:			printd_str("ACPI NVS\n");	break;
+	case E820_TYPE_UNUSABLE_DBG:		printd_str("unusable\n");	break;
+	case E820_TYPE_PMEM_DBG:		/* Fall through */
+	case E820_TYPE_PRAM_DBG:		printd_str("persistent mem\n");	break;
+	default:				printd_str("unknown\n");	break;
 	}
 }
 
@@ -42,8 +42,8 @@ static void e820_print_dbg(struct e820_entry_dbg *entries, int nr_entries)
 		printd_hex(entries[i].addr);
 		printd_str("-");
 		printd_hex(entries[i].addr + entries[i].size - 1);
+		printd_str("] ");
 		e820_print_type_dbg(entries[i].type);
-		printd_str("\n");
 	}
 }
 
