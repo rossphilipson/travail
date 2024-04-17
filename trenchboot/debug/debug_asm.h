c044d33c4f0f9457fea0c52ac3bc700c4f71a0a1
Ross Philipson <ross.philipson@oracle.com>
2024-04-17 09:17:09 -0700

    Add 64b version of asm debug macro
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/debug/debug_asm.h
+++ trenchboot/debug/debug_asm.h
@@ -26,4 +26,29 @@
 	popl	%ecx
 .endm
 
+.macro DEBUG64_PUTC char
+	pushq	%rcx
+	pushq	%rdx
+	pushq	%rax
+	xorq	%rcx, %rcx
+1:
+	cmpb	$5, %cl
+	jz	2f
+	movw	$0x3f8, %dx
+	addw	$5, %dx
+3:
+	inb	%dx, %al
+	testb	$0x20, %al
+	jz	3b
+	movw	$0x3f8, %dx
+	movb	\char, %al
+	outb	%al, %dx
+	incb	%cl
+	jmp	1b
+2:
+	popq	%rax
+	popq	%rdx
+	popq	%rcx
+.endm
+
 #endif
