604b4175a33d182607b0151a0f2892715591fc55
Ross Philipson <ross.philipson@oracle.com>
2024-04-17 08:35:12 -0700

    Add asm printer debug header
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

---
+++ trenchboot/debug/debug_asm.h
@@ -0,0 +1,29 @@
+#ifndef _DEBUG_ASM
+#define _DEBUG_ASM
+
+.macro DEBUG_PUTC char
+	pushl	%ecx
+	pushl	%edx
+	pushl	%eax
+	xorl	%ecx, %ecx
+1:
+	cmpb	$5, %cl
+	jz	2f
+	movw	$0x3f8, %dx
+	addw	$5, %dx
+3:
+	inb	%dx, %al
+	testb	$0x20, %al
+	jz	3b
+	movw	$0x3f8, %dx
+	movb	\char, %al
+	outb	%al, %dx
+	incb	%cl
+	jmp	1b
+2:
+	popl	%eax
+	popl	%edx
+	popl	%ecx
+.endm
+
+#endif
