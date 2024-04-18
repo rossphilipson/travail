7519a6e5e07accd33664a4583f455ab14350fac9
Ross Philipson <ross.philipson@oracle.com>
2024-04-18 11:03:22 -0700

    Use memory location instead of stack for asm debug putc functions
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- trenchboot/debug/debug_asm.h
+++ trenchboot/debug/debug_asm.h
@@ -1,10 +1,18 @@
 #ifndef _DEBUG_ASM
 #define _DEBUG_ASM
 
+#define DEBUG_MEMLOC 0x48000
+
 .macro DEBUG_PUTC char
+#ifdef DEBUG_USE_MEMLOC
+	movl	%ecx, (DEBUG_MEMLOC)
+	movl	%edx, (DEBUG_MEMLOC + 4)
+	movl	%eax, (DEBUG_MEMLOC + 8)
+#else
 	pushl	%ecx
 	pushl	%edx
 	pushl	%eax
+#endif
 	xorl	%ecx, %ecx
 1:
 	cmpb	$5, %cl
@@ -21,15 +29,27 @@
 	incb	%cl
 	jmp	1b
 2:
+#ifdef DEBUG_USE_MEMLOC
+	movl	(DEBUG_MEMLOC + 8), %eax
+	movl	(DEBUG_MEMLOC + 4), %edx
+	movl	(DEBUG_MEMLOC), %ecx
+#else
 	popl	%eax
 	popl	%edx
 	popl	%ecx
+#endif
 .endm
 
 .macro DEBUG64_PUTC char
+#ifdef DEBUG_USE_MEMLOC
+	movq	%rcx, (DEBUG_MEMLOC)
+	movq	%rdx, (DEBUG_MEMLOC + 8)
+	movq	%rax, (DEBUG_MEMLOC + 16)
+#else
 	pushq	%rcx
 	pushq	%rdx
 	pushq	%rax
+#endif
 	xorq	%rcx, %rcx
 1:
 	cmpb	$5, %cl
@@ -46,9 +66,15 @@
 	incb	%cl
 	jmp	1b
 2:
+#ifdef DEBUG_USE_MEMLOC
+	movq	(DEBUG_MEMLOC + 16), %rax
+	movq	(DEBUG_MEMLOC + 8), %rdx
+	movq	(DEBUG_MEMLOC), %rcx
+#else
 	popq	%rax
 	popq	%rdx
 	popq	%rcx
+#endif
 .endm
 
 #endif
